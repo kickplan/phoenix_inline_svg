b08425bb96b5aeff43b373fad306ab5277b764a8
robotloveskitten <tim@thinkcorps.com>
2023-10-05 16:20:29 -0500

    update floki

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PhoenixInlineSvg.Mixfile do
   use Mix.Project
 
-  @source_url "https://github.com/nikkomiu/phoenix_inline_svg"
+  @source_url "https://github.com/kickplan/phoenix_inline_svg"
   @version "1.4.0"
 
   def project do
@@ -35,7 +35,7 @@ defmodule PhoenixInlineSvg.Mixfile do
   defp deps do
     [
       {:phoenix, "~> 1.4"},
-      {:floki, "~> 0.24"},
+      {:floki, "~> 0.34.3"},
       {:inch_ex, "~> 1.0", only: [:dev, :test]},
       {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.5", only: [:dev, :test]},
